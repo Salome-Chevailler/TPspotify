98ef1e7936033706d3b8c69e9ea469c38818f415
Salome-Chevailler <74154174+Salome-Chevailler@users.noreply.github.com>
2021-02-05 09:44:48 +0100

    Afficher la liste des albums

--- spotify.js
+++ spotify.js
@@ -1,34 +1,57 @@
 // l'entête de la requête avec le token
-
 const headers = {
   "Content-Type": "application/json",
   Authorization:
-    "BQDZT-X9pihT7czh6zfjjEXWyIoQpXZ6mFI_cIj6wPWwrUuSIFuNRk0hRaGWHQ9irlT6-PxxRUsWnrRpfBPiBPH8lHKcAjWofvAcW3Ms-Fw2ayCTT2PDUDLqO7lQy9KtUZhE7yiMoQ"
+    "Bearer BQApiO02ijElcas_L7eDtJK_MWi-_xdNiqznrp8r65s2wg3pL7Wa-kknxyr3AAF0HTDSIGnuyBFPVutIsSiM8gcJOkHjKn11hn6XinDkrHEH11mTHPBHrnH5pmmiT6Sixihni00oLA"
 };
-
 const fetchOptions = { headers: headers };
 
 // événement validation (submit) du formulaire
-document.getElementById("fm").addEventListener("submit", chercherAlbums());
+document.getElementById("fm").addEventListener("submit", chercherAlbums);
 
 function chercherAlbums(event) {
-  // empêcher le rechargement de la page
+  // empecher le rechargement de la page
   event.preventDefault();
-  // le mot clé saisi
+  // le mot cle saisi
   let motcle = document.getElementById("motcle").value;
   let url =
     "https://api.spotify.com/v1/search?q=" +
     motcle +
     "&type=album&market=FR&limit=10";
 
-  // la requête AJAX
+  // la req AJAX
   fetch(url, fetchOptions)
     .then((response) => {
       return response.json();
     })
     .then((dataJSON) => {
       console.log(dataJSON);
-      let albums = dataJSON.albums.items;
+      afficheAlbums(dataJSON.albums.items);
     })
     .catch((error) => console.log(error));
+}
+
+// --------  afficher la liste des albums -----------------------------
+function afficheAlbums(albums) {
+  // le texte HTML à générer = une balise img par album
+  let resHTML = "";
+  for (let album of albums) {
+    resHTML =
+      resHTML +
+      '<img id="' +
+      album.id +
+      '" src="' +
+      album.images[1].url +
+      '"/>';
+  }
+  document.getElementById("albums").innerHTML = resHTML;
+
+  // gérer événement click sur les images
+  for (let elem of document.querySelectorAll("#albums > img")) {
+    elem.addEventListener("click", detailsAlbum);
+  }
+}
+
+function detailsAlbum(event) {
+  console.log(event.target.id);
 }
