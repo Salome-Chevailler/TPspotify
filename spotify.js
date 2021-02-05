eed8dec8284229edc00466b4abf98f76019d294f
Salome-Chevailler <74154174+Salome-Chevailler@users.noreply.github.com>
2021-02-05 08:58:30 +0100

    Initial commit

---
+++ spotify.js
@@ -0,0 +1,22 @@
+// l'entête de la requête avec le token 
+
+const headers ={
+  'Content-Type' : 'application/json', 
+  Authorization:
+    "BQDZT-X9pihT7czh6zfjjEXWyIoQpXZ6mFI_cIj6wPWwrUuSIFuNRk0hRaGWHQ9irlT6-PxxRUsWnrRpfBPiBPH8lHKcAjWofvAcW3Ms-Fw2ayCTT2PDUDLqO7lQy9KtUZhE7yiMoQ"
+};
+
+const fetchOptions = {headers: headers};
+
+// événement validation (submit) du formulaire
+document.getElementById("fm").addEventListener("submit", chercherAlbums());
+
+function chercherAlbums(event) {
+  // empêcher le rechargement de la page
+  event.preventDefault();
+  // le mot clé saisi
+  let motcle = document.getElementById("motcle").value;
+  let url = "https://api.spotify.com/v1/search?q=motcle&type=album&market=FR&limit=10";
+
+  // la requête AJAX
+}
