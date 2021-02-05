6c92ff5847229d20c0f06d8c089cdf7d731df7e9
Salome-Chevailler <74154174+Salome-Chevailler@users.noreply.github.com>
2021-02-05 09:10:57 +0100

    Requête AJAX

--- spotify.js
+++ spotify.js
@@ -1,12 +1,12 @@
-// l'entête de la requête avec le token 
+// l'entête de la requête avec le token
 
-const headers ={
-  'Content-Type' : 'application/json', 
+const headers = {
+  "Content-Type": "application/json",
   Authorization:
     "BQDZT-X9pihT7czh6zfjjEXWyIoQpXZ6mFI_cIj6wPWwrUuSIFuNRk0hRaGWHQ9irlT6-PxxRUsWnrRpfBPiBPH8lHKcAjWofvAcW3Ms-Fw2ayCTT2PDUDLqO7lQy9KtUZhE7yiMoQ"
 };
 
-const fetchOptions = {headers: headers};
+const fetchOptions = { headers: headers };
 
 // événement validation (submit) du formulaire
 document.getElementById("fm").addEventListener("submit", chercherAlbums());
@@ -16,7 +16,19 @@ function chercherAlbums(event) {
   event.preventDefault();
   // le mot clé saisi
   let motcle = document.getElementById("motcle").value;
-  let url = "https://api.spotify.com/v1/search?q=motcle&type=album&market=FR&limit=10";
+  let url =
+    "https://api.spotify.com/v1/search?q=" +
+    motcle +
+    "&type=album&market=FR&limit=10";
 
   // la requête AJAX
+  fetch(url, fetchOptions)
+    .then((response) => {
+      return response.json();
+    })
+    .then((dataJSON) => {
+      console.log(dataJSON);
+      let albums = dataJSON.albums.items;
+    })
+    .catch((error) => console.log(error));
 }
